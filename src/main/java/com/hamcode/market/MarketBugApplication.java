cd900e501f64eabcf15da138decd95384a77de45
John Doe <mamanialdo98@hotmail.com>
2019-09-14 16:28:06 -0500

    Proyecto Inicial

---
+++ src/main/java/com/hamcode/market/MarketBugApplication.java
@@ -0,0 +1,13 @@
+package com.hamcode.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketBugApplication.class, args);
+	}
+
+}
